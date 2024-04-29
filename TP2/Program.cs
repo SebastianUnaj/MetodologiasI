2c4bcb7e3ea39c115fc1ee296a7a587f71a6e8d2
Sebastian Cacere <sebastianunaj@gmail.com>
2024-04-29 17:33:17 -0300

    TP2-Ejercicio 2. creo alumno con estrategias varias informo alumnos en una coleccion multiple

--- TP2/Program.cs
+++ TP2/Program.cs
@@ -44,8 +44,118 @@ namespace practica2
 			Console.WriteLine("\tSos menor: "+a1.sosMenor(a2));
 			Console.WriteLine("\tSos mayor: "+a1.sosMayor(a2));
 			
+			//************EJERCICIO 2*****************/
+			
+			//coleccion multiple de Alumnos
+			
+			Pila p2= new Pila();
+			Cola col2=new Cola();
+			ColeccionMultiple cM2= new ColeccionMultiple(p2, col2);
+			llenarAlumnos(p2);
+			llenarAlumnos(col2);
+			Console.WriteLine("INFORMO COLECCION MULTIPLE de Alumnos");
+			//Type myType = typeof(practica2.Program);
+        	 //Console.WriteLine("   Namespace: {0}.", myType.Namespace);
+			Console.WriteLine("tipo coleccion multiple: "+ cM2.GetType().ToString());
+			informar(cM2);
+			
+			
 			Console.Write("Press any key to continue . . . ");
 			Console.ReadKey(true);
 		}
+		
+		
+		
+		
+		/// <summary>
+		/// METODOS STATICOS
+		/// </summary>
+		/// <param name="c"></param>
+		 
+		private static Random unicoRandomGlobal = new Random();
+		
+		public static void llenar(coleccionable c){
+			for (int i = 0; i < 20; i++) {
+				int auxi= (new RandomUnico(unicoRandomGlobal)).numeroRandom();
+					Numero aux= new Numero(auxi);
+				comparable a= aux;
+				c.agregar(a);
+			}
+		}//fin llenar
+		
+		public static void informar(coleccionable c){
+			Console.WriteLine("Cantidad de elementos: "+c.cuantos());
+			//var tipo;
+			switch (c.minimo().GetType().ToString()) {
+				case "practica2.Numero":
+					Console.WriteLine("Valor Mínimo: "+((Numero)c.minimo()).getValor);
+					break;
+				case "practica2.Persona":
+					Console.WriteLine("Nombre Mínimo: "+((Persona)c.minimo()).getNombre);
+					break;
+				case "practica2.Alumno":
+					Console.WriteLine("Legajo Mínimo: "+((Alumno)c.minimo()).getLegajo);
+					break;
+			}
+			switch (c.maximo().GetType().ToString()) {
+				case "practica2.Numero":
+					Console.WriteLine("Valor Máximo: "+((Numero)c.maximo()).getValor);
+					break;
+				case "practica2.Persona":
+					Console.WriteLine("Nombre Máximo: "+((Persona)c.maximo()).getNombre);
+					break;
+				case "practica2.Alumno":
+					Console.WriteLine("Legajo Máximo: "+((Alumno)c.maximo()).getLegajo);
+					break;
+			}
+			Console.Write("contiene: ");
+			string contien= Console.ReadLine();
+			comparable a=null;
+			
+			switch (c.maximo().GetType().ToString()) {
+				case "practica2.Numero":
+					a= new Numero(Int32.Parse(contien));
+					break;
+				case "practica2.Persona":
+					a= new Persona(contien, 11111111);
+					break;
+				case "practica2.Alumno":
+					a= new Alumno("comparale",11111111,Int32.Parse(contien),double.Parse(contien));
+					break;
+			}
+			
+			if (c.cotiene(a))
+				Console.WriteLine("El elemento leído está en la colección");
+			else
+				Console.WriteLine("El elemento leído NO está en la colección");
+			
+		}//fin informar
+		
+		public static void llenarPersonas(coleccionable col){
+			for (int i = 0; i < 10; i++) {
+				RandomUnico r= new RandomUnico(unicoRandomGlobal);
+				int dni= r.dniRandom();
+				int np= r.numeroRandom();
+				string nom= "Persona "+np;
+				comparable comp= new Persona (nom, dni);
+				col.agregar(comp);
+			}
+		}//llenarPersonas
+		
+		public static void llenarAlumnos(coleccionable col){
+			for (int i = 0; i < 10; i++) {
+				RandomUnico r= new RandomUnico(unicoRandomGlobal);
+				int dni= r.dniRandom();
+				int np= r.numeroRandom();
+				string nom= "Alumno "+np;
+				int legaj= r.legajoRandom();
+				double prom= r.promedioRandom();
+				Alumno AlumAux= new Alumno (nom, dni, legaj, prom);
+				int strategia= (i*3)%4+1;//para obtener valores alternos entre 1 y 4 que cambien la estrategia
+				AlumAux.cambiarComparar(strategia.ToString());
+				comparable comp= AlumAux;
+				col.agregar(comp);
+			}
+		}//llenarAlumnos
 	}
 }
